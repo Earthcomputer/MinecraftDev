c8c019960ec01cd566110e81272164ea4bf4769c
Joe <burtonjae@hotmail.co.uk>
2020-02-28 16:20:44 +0000

    Handle invokers for constructors

--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/util/Invoker.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/util/Invoker.kt
@@ -10,15 +10,12 @@
 
 package com.demonwav.mcdev.platform.mixin.util
 
-import com.demonwav.mcdev.util.constantStringValue
-import com.demonwav.mcdev.util.findAnnotation
-import com.demonwav.mcdev.util.findMatchingMethod
-import com.demonwav.mcdev.util.ifEmpty
-import com.demonwav.mcdev.util.mapFirstNotNull
+import com.demonwav.mcdev.util.*
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiMember
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiUtil
 import org.jetbrains.annotations.Contract
 import java.util.*
 
@@ -37,8 +34,18 @@ fun resolveInvokerTarget(
         member: PsiMember
 ): PsiMember? {
     val name = getInvokerTargetName(invoker, member) ?: return null
+    val constructor = name == "<init>"
     return when (member) {
-        is PsiMethod -> targetClasses.mapFirstNotNull { it.findMatchingMethod(member, false, name) }
+        is PsiMethod -> targetClasses.mapFirstNotNull {
+            if (constructor && PsiUtil.resolveClassInType(member.returnType)?.qualifiedName != it.qualifiedName)
+                return null
+            it.findMatchingMethod(
+                member,
+                false,
+                if (constructor) it.name ?: name else name,
+                constructor
+            )
+        }
         else -> null
     }
 }
