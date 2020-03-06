e6ea49447a0d62d67a7ad75f81fb9906dd014de6
Joe <burtonjae@hotmail.co.uk>
2020-03-06 17:41:30 +0000

    Basic accessor generators

--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/MixinModule.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/MixinModule.kt
@@ -10,14 +10,17 @@
 
 package com.demonwav.mcdev.platform.mixin
 
-import com.demonwav.mcdev.buildsystem.SourceType
 import com.demonwav.mcdev.facet.MinecraftFacet
 import com.demonwav.mcdev.platform.AbstractModule
 import com.demonwav.mcdev.platform.PlatformType
+import com.demonwav.mcdev.platform.mixin.util.MixinConfig
+import com.intellij.json.psi.JsonFile
+import com.intellij.json.psi.JsonObject
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.fileTypes.FileTypes
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFile
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
@@ -25,14 +28,33 @@ import javax.swing.Icon
 
 class MixinModule(facet: MinecraftFacet) : AbstractModule(facet) {
 
-    private val mixinFileType = FileTypeManager.getInstance().findFileTypeByName("Mixin Configuration") ?: FileTypes.UNKNOWN
-
     override val moduleType = MixinModuleType
     override val type = PlatformType.MIXIN
     override val icon: Icon? = null
 
-    fun getMixinConfigs(project: Project, scope: GlobalSearchScope): Collection<PsiFile> {
-        return FileTypeIndex.getFiles(mixinFileType, scope).mapNotNull { PsiManager.getInstance(project).findFile(it) }
+    companion object {
+        private val mixinFileType = FileTypeManager.getInstance().findFileTypeByName("Mixin Configuration") ?: FileTypes.UNKNOWN
+
+        fun getMixinConfigs(project: Project, scope: GlobalSearchScope): Collection<MixinConfig> {
+            return FileTypeIndex.getFiles(mixinFileType, scope).
+                    mapNotNull { (PsiManager.getInstance(project).findFile(it) as? JsonFile)?.topLevelValue as? JsonObject }.
+                    map { MixinConfig(project, it) }
+        }
+
+        fun getAllMixins(project: Project, scope: GlobalSearchScope): Collection<PsiClass> {
+            return getMixinConfigs(project, scope).asSequence().
+                    flatMap { (it.qualifiedMixins + it.qualifiedClient + it.qualifiedServer).asSequence() }.
+                    filterNotNull().
+                    distinct().
+                    flatMap { JavaPsiFacade.getInstance(project).findClasses(it, scope).asSequence() }.
+                    toList()
+        }
+
+        fun getBestWritableConfigForMixinClass(project: Project, scope: GlobalSearchScope, mixinClassName: String): MixinConfig? {
+            return getMixinConfigs(project, scope).
+                    filter { it.isWritable && mixinClassName.startsWith("${it.pkg}.") }.
+                    maxBy { it.pkg?.length ?: 0 }
+        }
     }
 
 }
--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/UnusedMixinInspection.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/UnusedMixinInspection.kt
@@ -10,25 +10,22 @@
 
 package com.demonwav.mcdev.platform.mixin.inspection
 
-import com.demonwav.mcdev.facet.MinecraftFacet
 import com.demonwav.mcdev.inspection.sideonly.Side
 import com.demonwav.mcdev.inspection.sideonly.SideOnlyUtil
-import com.demonwav.mcdev.platform.mixin.MixinModuleType
-import com.demonwav.mcdev.platform.mixin.config.reference.MixinClass
+import com.demonwav.mcdev.platform.mixin.util.MixinConfig
+import com.demonwav.mcdev.platform.mixin.MixinModule
 import com.demonwav.mcdev.platform.mixin.util.isMixin
 import com.demonwav.mcdev.util.findModule
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.ide.util.EditorHelper
 import com.intellij.json.psi.*
-import com.intellij.lang.LanguageImportStatements
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.search.GlobalSearchScope
 
 class UnusedMixinInspection : MixinInspection() {
 
@@ -39,40 +36,23 @@ class UnusedMixinInspection : MixinInspection() {
     class Visitor(private val holder: ProblemsHolder) : JavaElementVisitor() {
         override fun visitClass(clazz: PsiClass?) {
             val module = clazz?.findModule() ?: return
-            val facet = MinecraftFacet.getInstance(module) ?: return
-            val mixinModule = facet.getModuleOfType(MixinModuleType) ?: return
             if (clazz.isMixin) {
-                var bestQuickFixConfig: PsiFile? = null
-                var longestPackageLength = Integer.MIN_VALUE
-                for (config in mixinModule.getMixinConfigs(module.project, clazz.resolveScope)) {
-                    val root = (config as? JsonFile)?.topLevelValue as? JsonObject ?: continue
-
-                    if (referencesClass(root.findProperty("mixins"), clazz))
+                for (config in MixinModule.getMixinConfigs(module.project, GlobalSearchScope.moduleScope(module))) {
+                    if (config.qualifiedMixins.any { it == clazz.qualifiedName })
                         return
-                    if (referencesClass(root.findProperty("client"), clazz))
+                    if (config.qualifiedClient.any { it == clazz.qualifiedName })
                         return
-                    if (referencesClass(root.findProperty("server"), clazz))
+                    if (config.qualifiedServer.any { it == clazz.qualifiedName })
                         return
-
-                    val pkg = (root.findProperty("package")?.value as? JsonStringLiteral)?.value ?: continue
-                    if (clazz.qualifiedName?.startsWith("$pkg.") != true) continue
-                    if (config.isWritable && pkg.length > longestPackageLength) {
-                        bestQuickFixConfig = config
-                        longestPackageLength = pkg.length
-                    }
                 }
 
+                val bestQuickFixConfig = MixinModule.getBestWritableConfigForMixinClass(module.project, GlobalSearchScope.moduleScope(module), clazz.qualifiedName ?: "")
                 val problematicElement = clazz.nameIdentifier
                 if (problematicElement != null) {
-                    val bestQuickFixFile = bestQuickFixConfig?.virtualFile
+                    val bestQuickFixFile = bestQuickFixConfig?.file
                     val qualifiedName = clazz.qualifiedName
                     if (bestQuickFixFile != null && qualifiedName != null) {
-                        val type = when (SideOnlyUtil.getSideForClass(clazz)) {
-                            Side.CLIENT -> "client"
-                            Side.SERVER -> "server"
-                            else -> "mixins"
-                        }
-                        val quickFix = QuickFix(bestQuickFixFile, qualifiedName, type)
+                        val quickFix = QuickFix(bestQuickFixFile, qualifiedName, SideOnlyUtil.getSideForClass(clazz))
                         holder.registerProblem(problematicElement, "Mixin not found in any mixin config", quickFix)
                     } else {
                         holder.registerProblem(problematicElement, "Mixin not found in any mixin config")
@@ -80,39 +60,21 @@ class UnusedMixinInspection : MixinInspection() {
                 }
             }
         }
-
-        private fun referencesClass(property: JsonProperty?, clazz: PsiClass): Boolean {
-            val values = property?.value as? JsonArray ?: return false
-            return values.valueList.any valueTester@{ v ->
-                val pkg = MixinClass.getBasePackage(v) ?: return@valueTester false
-                val strVal = v as? JsonStringLiteral ?: return@valueTester false
-                "$pkg.${strVal.value}" == clazz.qualifiedName
-            }
-        }
     }
 
-    private class QuickFix(private val quickFixFile: VirtualFile, private val qualifiedName: String, private val type: String) : LocalQuickFix {
+    private class QuickFix(private val quickFixFile: VirtualFile, private val qualifiedName: String, private val side: Side) : LocalQuickFix {
         override fun getName() = "Add to mixin config"
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
             val psiFile = PsiManager.getInstance(project).findFile(quickFixFile) as? JsonFile ?: return
             val root = psiFile.topLevelValue as? JsonObject ?: return
-            val pkg = (root.findProperty("package")?.value as? JsonStringLiteral)?.value ?: return
-            var mixinArray = root.findProperty(type)?.value as? JsonArray
-            if (mixinArray == null) {
-                val added = JsonPsiUtil.addProperty(root, JsonElementGenerator(project).createProperty(type, "[]"), false)
-                mixinArray = (added as? JsonProperty)?.value as? JsonArray
-                if (mixinArray == null)
-                    return
+            val config = MixinConfig(project, root)
+            val mixinList = when (side) {
+                Side.CLIENT -> config.qualifiedClient
+                Side.SERVER -> config.qualifiedServer
+                else -> config.qualifiedMixins
             }
-            val name = qualifiedName.substring(pkg.length + 1)
-            if (mixinArray.valueList.isNotEmpty()) {
-                mixinArray.addBefore(JsonElementGenerator(project).createComma(), mixinArray.lastChild)
-            }
-            mixinArray.addBefore(JsonElementGenerator(project).createStringLiteral(name), mixinArray.lastChild)
-            LanguageImportStatements.INSTANCE.forFile(psiFile).forEach { it.processFile(psiFile).run() }
-            val addedValue = ((psiFile.topLevelValue as? JsonObject)?.findProperty(type)?.value as? JsonArray)?.valueList?.first { (it as? JsonStringLiteral)?.value == name } ?: psiFile
-            EditorHelper.openInEditor(addedValue, true, true)
+            mixinList.add(qualifiedName)
         }
 
         override fun getFamilyName() = name
